142d1b54e671abe6d411a6e7b3e636ed717113e0
Richard Ellicott <mescalin@gmail.com>
2025-10-16 05:06:17 +0100

    working cross compile now, compiles on wsl and windows

--- BUILD.sh
+++ BUILD.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-set -e
+set -e # Exit on error
 
 export CPLUS_INCLUDE_PATH=/usr/include/python3.12
-# export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu
 
-mkdir -p build
-cd build
+# might need to beware of line endings (although i set vs.code now to use \n)
+# dos2unix BUILD.sh
 
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release
-ninja
+BUILD_DIR="build/linux"
+mkdir -p $BUILD_DIR
+cmake -S . -B "$BUILD_DIR" -G Ninja -DCMAKE_BUILD_TYPE=Release
+ninja -C "$BUILD_DIR"
+
+
+
 
 
 
---
+++ GIT_COMMANDS.md
@@ -0,0 +1,12 @@
+# Git Commands
+
+## Add to repo
+
+```powershell
+git status # what changes?
+git add .
+git status # look again will go green
+git commit -m "Describe your changes here" # make a commit
+git push origin windows-rebuild # finally push
+```
+
